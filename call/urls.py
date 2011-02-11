e63d588df5bdaff5617854626562e933cc2de5ab
Matt Howell <misterinterrupt@gmail.com>
2011-02-11 02:35:06 -0800

    added simple hello monkey message to /listen

--- call/urls.py
+++ call/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
-from call.views import index
+from call.views import index, listen
 urlpatterns = patterns('',
     #home
     url(r'^/?$', index, name='call_index'),
+    url(r'^listen/?$', listen, name='call_listen'),
 )
--- call/views.py
+++ call/views.py
@@ -1,3 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from xml.dom import minidom
 def index(response):
     return HttpResponse("Call Index")
+    
+def listen(response):
+    return render_to_response("call/hello.xml")
