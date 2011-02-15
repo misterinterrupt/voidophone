5cbc371c0ac78e1d729b28fa2ab1463d21d0a617
Matt Howell <misterinterrupt@gmail.com>
2011-02-15 00:32:06 -0800

    modified call url to play external mp3

--- call/views.py
+++ call/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
-from xml.dom import minidom
-def index(response):
+import twilio
+import scapi
+
+MP3_URL = 'http://soundcloud.com/misterinterrupt/spectralis/download'
+
+def index(request):
     return HttpResponse("Call Index")
     
-def listen(response):
-    return render_to_response("call/hello.xml")
+def listen(request):
+    response = twilio.Response()
+    response.append(twilio.Say("You have reached the void. Now listen to spectralis by misterinterrupt", voice=twilio.Say.WOMAN, 
+        language=twilio.Say.ENGLISH))
+    response.addPlay(MP3_URL)
+    return HttpResponse(response)
