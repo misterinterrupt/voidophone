3ffcafaa17e75a6e0aab62263e728ebb91ca253b
Matt Howell <misterinterrupt@gmail.com>
2011-02-16 02:22:38 -0800

    added url for process_index

--- voidophone/call/urls.py
+++ voidophone/call/urls.py
@@ -3,6 +3,7 @@ from voidophone.call.views import index, listen, record
 urlpatterns = patterns('',
     #home
     url(r'^/?$', index, name='call_index'),
+    url(r'^/process_index/?$', process_index, name='call_process_index')
     url(r'^listen/?$', listen, name='call_listen'),
     url(r'^record/?$', record, name='call_record')
 )
